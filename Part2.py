7f15fa8dfc706dea9c6a54b45932b1adc4a9c839
e183133h <62248381+e183133h@users.noreply.github.com>
2020-04-03 16:17:20 +0200

    Le B, C, D effectué et le A me brise les burnes

--- Part2.py
+++ Part2.py
@@ -3,79 +3,78 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Ouvrir les fichiers csv
+# Alias
+file_deads = 'time_series_covid19_deaths_global.csv'
+file_cases = 'time_series_covid19_confirmed_global.csv'
+pays = ['Italy', 'Spain', 'France', 'Hubei (China)', 'US', 'Netherlands', 'Jiangxi (China)', 'Singapore', 'Argentina']
 
 ## Pays à analyser : Italy (137), Spain (202), France (116), Hubei (China) (62), US (226), Netherlands (170), Jiangxi (China) (66), Singapore (197), Argentina (6)
 
-################### CAS ###################
-file_cases = 'time_series_covid19_confirmed_global.csv'
+################### Question A ###################
+data_confirmed_cases = pd.read_csv(file_cases).loc[[137, 202, 116, 62, 226, 170, 66, 197, 6]]
+data_confirmed_cases.drop(['Lat', 'Long', 'Province/State', 'Country/Region'], axis='columns', inplace=True)
+data_death_cases = pd.read_csv(file_deads).loc[[137, 202, 116, 62, 226, 170, 66, 197, 6]]
+data_death_cases.drop(['Lat', 'Long', 'Province/State', 'Country/Region'], axis='columns', inplace=True)
+i=0
+for idx, rows1 in data_death_cases.iterrows():
+    fig, ax = plt.subplots(figsize=(17, 8))
+    plt.plot(rows1, color="orange", label="morts")
+    for idy, rows2 in data_confirmed_cases.iterrows():
+        if(idx == idy):
+            plt.plot(rows2, color="blue", label="contaminés")
+    plt.xlabel("Dates")
+    plt.ylabel("Nombre de gens")
+    plt.title("Nombre journalier de cas et de morts: "+pays[i])
+    plt.setp(ax.get_xticklabels(), rotation=88)
+    plt.legend()
+    i=i+1
 
+plt.show()
 
-##### Question b) #####
+################### Question B ###################
 # data_confirmed_cases = pd.read_csv(file_cases).loc[[137, 202, 116, 62, 226, 170, 66, 197, 6]]
 # data_confirmed_cases.drop(['Lat', 'Long', 'Province/State', 'Country/Region'], axis='columns', inplace=True)
-# print(data_confirmed_cases)
-#
-#
+# data_death_cases = pd.read_csv(file_deads).loc[[137, 202, 116, 62, 226, 170, 66, 197, 6]]
+# data_death_cases.drop(['Lat', 'Long', 'Province/State', 'Country/Region'], axis='columns', inplace=True)
 # i=0
-# pays = ['Italy', 'Spain', 'France', 'Hubei (China)', 'US', 'Netherlands', 'Jiangxi (China)', 'Singapore', 'Argentina']
-# for idx, rows in data_confirmed_cases.iterrows():
+# for idx, rows1 in data_death_cases.iterrows():
 #     fig, ax = plt.subplots(figsize=(17, 8))
-#     plt.plot(rows)
+#     plt.plot(rows1, color="orange", label="total morts")
+#     for idy, rows2 in data_confirmed_cases.iterrows():
+#         if(idx == idy):
+#             plt.plot(rows2, color="blue", label="total contaminés")
 #     plt.xlabel("Dates")
-#     plt.ylabel("Nombre de cas confirmé")
-#     plt.title(pays[i])
+#     plt.ylabel("Nombre de gens")
+#     plt.title("Nombre total de cas et de morts: "+pays[i])
 #     plt.setp(ax.get_xticklabels(), rotation=88)
+#     plt.legend()
 #     i=i+1
 #
 # plt.show()
 
 
-##### Question c) #####
-# data_confirmed_cases = pd.read_csv(file_cases).loc[[137, 202, 116, 62, 226, 170, 66, 197, 6]]
-# data_confirmed_cases.drop(['Lat', 'Long', 'Province/State', 'Country/Region'], axis='columns', inplace=True)
-# print(data_confirmed_cases)
+###############Question C#################
+# data_death_cases = pd.read_csv(file_cases).loc[[137, 202, 116, 62, 226, 170, 66, 197, 6]]
+# data_death_cases.drop(['Lat', 'Long', 'Province/State', 'Country/Region'], axis='columns', inplace=True)
+# print(data_death_cases)
 #
 # pays = ['Italy', 'Spain', 'France', 'Hubei (China)', 'US', 'Netherlands', 'Jiangxi (China)', 'Singapore', 'Argentina']
 #
 # fig, ax = plt.subplots(figsize=(17, 8))
-# i=0
-# for idx, rows in data_confirmed_cases.iterrows():
-#     plt.plot(rows, label=pays[i])
+# j=0
+# for idx, rows in data_death_cases.iterrows():
+#     plt.plot(rows, label=pays[j])
 #     plt.setp(ax.get_xticklabels(), rotation=88)
-#     i=i+1
+#     j=j+1
 #
 #
 #
 # plt.xlabel("Dates")
-# plt.ylabel("Nombre de cas confirmé")
+# plt.ylabel("Nombre de cas confirmés")
 # plt.legend()
 # plt.show()
 
-print("\n")
-################### MORTS ###################
-file_deads = 'time_series_covid19_deaths_global.csv'
-
-
-##### Question b) #####
-# data_death_cases = pd.read_csv(file_deads).loc[[137, 202, 116, 62, 226, 170, 66, 197, 6]]
-# data_death_cases.drop(['Lat', 'Long', 'Province/State', 'Country/Region'], axis='columns', inplace=True)
-# print(data_death_cases)
-#
-# j=0
-# for idx, rows in data_death_cases.iterrows():
-#     fig, ax = plt.subplots(figsize=(17, 8))
-#     plt.plot(rows)
-#     plt.xlabel("Dates")
-#     plt.ylabel("Nombre de morts")
-#     plt.title(pays[j])
-#     plt.setp(ax.get_xticklabels(), rotation=88)
-#     j=j+1
-#
-# plt.show()
-
-
-##### Question d) #####
+###############Question D#################
 # data_death_cases = pd.read_csv(file_deads).loc[[137, 202, 116, 62, 226, 170, 66, 197, 6]]
 # data_death_cases.drop(['Lat', 'Long', 'Province/State', 'Country/Region'], axis='columns', inplace=True)
 # print(data_death_cases)
@@ -92,6 +91,6 @@ file_deads = 'time_series_covid19_deaths_global.csv'
 #
 #
 # plt.xlabel("Dates")
-# plt.ylabel("Nombre de cas confirmé")
+# plt.ylabel("Nombre de cas décès")
 # plt.legend()
 # plt.show()
